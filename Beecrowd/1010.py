06dffbb70d20539b0703c670880e15d5059d050f
IsaacBites <isaacbites80@gmail.com>
2023-11-03 18:40:59 -0200

    Update Beecrowd
    Criação do exercício 1010.py

---
+++ Beecrowd/1010.py
@@ -0,0 +1,11 @@
+cod1 = int(input())
+qtd1 = int(input())
+valor1 = float(input())
+
+cod2 = int(input())
+qtd2 = int(input())
+valor2 = float(input())
+
+total = (qtd1 * valor1) + (qtd2 * valor2)
+
+print('VALOR A PAGAR: R$ ' f"{total:.2f}")
