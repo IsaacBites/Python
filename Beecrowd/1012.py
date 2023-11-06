bd7f7ec4e2e6fefbb5e03856f992be1cbaa0df1f
IsaacBites <isaacbites80@gmail.com>
2023-11-06 18:26:02 -0200

    Update Beecrowd
    Criação do exercício 1012.py

---
+++ Beecrowd/1012.py
@@ -0,0 +1,15 @@
+A = input()
+B = input()
+C = input()
+
+triangulo = (A * C)/2
+circulo = 3.14159 * (C ** 2)
+trapezio = (A + B) * C/ 2
+quadrado = B ** 2
+retangulo = A * B
+
+print('TRIANGULO: ' f"{triangulo:.3f}")
+print('CIRCULO: ' f"{circulo:.3f}")
+print('TRAPEZIO: ' f"{trapezio:.3f}")
+print('QUADRADO: ' f"{quadrado:.3f}")
+print('RETANGULO: ' f"{retangulo:.3f}")
