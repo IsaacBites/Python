f2968c6d44c9bf9dfe944852b3c7566e56ebad1e
IsaacBites <isaacbites80@gmail.com>
2023-11-06 18:58:57 -0200

    Update Beecrowd
    Criação do exercício 1015.py

---
+++ Beecrowd/1015.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+x1 = float(input())
+y1 = float(input())
+
+x2 = float(input())
+y2 = float(input())
+
+raiz = math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+
+print(f"{raiz:.4f}")
