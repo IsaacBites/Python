abde4b3a7c1adca38b4d5864284cf086356e2cb6
IsaacBites <isaacbites80@gmail.com>
2023-11-14 18:53:10 -0200

    Update Beecrowd
    Criação do exercício 1018.py

---
+++ Beecrowd/1018.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+valor = int(input())
+
+notas_100 = notas_50 = notas_20 = notas_10 = notas_5 = notas_2 = notas_1 = 0
+total = valor
+
+notas_100 = valor // 100
+valor %= 100
+
+notas_50 = valor // 50
+valor %= 50
+
+notas_20 = valor // 20
+valor %= 20
+
+notas_10 = valor // 10
+valor %= 10
+
+notas_5 = valor // 5
+valor %= 5
+
+notas_2 = valor // 2
+valor %= 2
+
+notas_1 = valor
+
+print(total)
+print(f"{notas_100} nota(s) de R$ 100,00")
+print(f"{notas_50} nota(s) de R$ 50,00")
+print(f"{notas_20} nota(s) de R$ 20,00")
+print(f"{notas_10} nota(s) de R$ 10,00")
+print(f"{notas_5} nota(s) de R$ 5,00")
+print(f"{notas_2} nota(s) de R$ 2,00")
+print(f"{notas_1} nota(s) de R$ 1,00")
