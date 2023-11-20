492efbf987594ac78bc01e2857efe0bd86a295c3
IsaacBites <isaacbites80@gmail.com>
2023-11-20 05:27:29 -0200

    Update Beecrowd
    Criação do exercício 1035.py

---
+++ Beecrowd/1035.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+A = input()
+B = int(input())
+C = int(input())
+D = int(input())
+
+if((B > C) and (D > A) and (D+C > A+B) and (C > 0) and (D > 0) and (A %2==0)):
+    print('Valores aceitos')
+else:
+    print('Valores nao aceitos')
