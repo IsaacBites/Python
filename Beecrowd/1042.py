5e01c90027a8790e659fbfb4bdc4aed035179f43
IsaacBites <isaacbites80@gmail.com>
2023-12-10 09:59:24 -0200

    Update Beecrowd
    Criação do exercício 1042.py

---
+++ Beecrowd/1042.py
@@ -0,0 +1,17 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+menor = min(n1, n2, n3)
+maior = max(n1, n2, n3)
+meio = (n1 + n2 + n3) - menor - maior
+
+print(menor)
+print(meio)
+print(maior)
+
+print("")
+
+print(maior)
+print(meio)
+print(menor)
