cb8ac4b9fbbd0c879348d4e6a2a3291b0e768843
IsaacBites <isaacbites80@gmail.com>
2024-01-16 13:12:51 -0200

    Update Beecrowd
    Criação do exercício 1052.py

---
+++ Beecrowd/1052.py
@@ -0,0 +1,21 @@
+meses = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+}
+
+x = int(input())
+
+if 1 <= x <= 12:
+    print(meses[x])
+else:
+    print('Número inválido')
